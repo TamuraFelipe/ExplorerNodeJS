d696f39e6ff86d2e2498f56e57af9d4061fc6a5e
Tamura <tamurafelipe@gmail.com>
2022-12-21 12:22:13 -0300

    Migrations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
-
 const routes = Router();
-
 /*Agrupador de rogas*/
 const usersRouter = require('./users.routes');
+const notesRouter = require('./notes.routes');
+
+
+
 
 /*Rota leva para as requisições*/
 routes.use("/users", usersRouter);
+routes.use("/notes", notesRouter);
 
 module.exports = routes;
